8d8c691aab480f484f903e27e111848437d80825
SeregaL2001 <92082104+SeregaL2001@users.noreply.github.com>
2022-12-29 16:40:00 +0300

    не допиленная прога на какой то стадии

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 
 int main(int argc, char *argv[])
 {
-    double h, alpha, p1_0, p2_0, B_0;
+    double h, alpha, x2, p1, p2, B;
     double error;
     Point *head = (Point *)malloc(sizeof(head));
 
     printf("Введите параметр a: ");
     scanf("%lf", &alpha);
 
+    x2 = find_x2_or_p1(X_10, P_2, alpha, 0);
+    p1 = find_x2_or_p1(X_10, P_2, alpha, 1);
+
+     RungeKutta(&head, X_10, x2, p1, P_2, alpha, 0);
+
+    printf("Integral = %.11f\n", Integral(head));
 
     clear_list(&head);
     return 0;
--- method.hpp
+++ method.hpp
@@ -7,8 +7,10 @@
 
 #define EPS 1e-13
 #define EPS1 1e-10
-#define X0 1 // x(0) = 1 
-#define P_1 0 // p(1) = 0
+#define P_1 0 // p1(1) = 0
+#define P_2 0 // p2(1) = 0
+#define X_10 1 //
+#define DELTA 1e-6
 
 #define PI 3.14159265358979323846
 
@@ -30,11 +32,11 @@ void clear_list(Point **head); // очистка списка
 void print_list(const Point *head); // печать списка в терминале 
 void fprint_list(const Point *head, FILE *fp); // печать списка в файл 
 
-double f1(double x1, double x2, double p1, double p2, double t, double alpha); // правая часть первого уравнения краевой задачи 
-double f2(double x1, double x2, double p1, double p2, double t, double alpha); // правая часть 2ого уравнения краевой задачи 
-double f3(double x1, double x2, double p1, double p2, double t, double alpha); // правая часть 3его уравнения краевой задачи 
-double f4(double x1, double x2, double p1, double p2, double t, double alpha); // правая часть 4ого уравнения краевой задачи 
+double f1(double t, double x1, double x2, double p1, double p2, double alpha); // правая часть первого уравнения краевой задачи 
+double f2(double t, double x1, double x2, double p1, double p2, double alpha); // правая часть 2ого уравнения краевой задачи 
+double f3(double t, double x1, double x2, double p1, double p2, double alpha); // правая часть 3его уравнения краевой задачи 
+double f4(double t, double x1, double x2, double p1, double p2, double alpha); // правая часть 4ого уравнения краевой задачи 
 
-double RungeKutta(Point** head, double x1_0, double x2_0, double p1_0, double p2_0, double alpha); // метод Рунге Кутты пятого порядка, с формулами построенными Инглэндом стр 31-32 Арушанян 
-double find_p0(double x0, double alpha); // поиск краевого условия методом пристрелки с помощью Ньютона 
+double RungeKutta(Point** head, double x1_0, double x2_0, double p1_0, double p2_0, double alpha, int check); // метод Рунге Кутты пятого порядка, с формулами построенными Инглэндом стр 31-32 Арушанян 
+double find_x2_or_p1(double x1_0, double p2_0, double alpha, int mark); // поиск краевого условия методом пристрелки с помощью Ньютона 
 double Integral(Point* head); // подсчет интеграла 
